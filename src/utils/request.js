42b4b30bfd72ed3070c0264502e6cfaba4c81969
Sergey Gromov <reskwer@ya.ru>
2018-04-23 11:06:59 +0300

    sync проблемы с CORS

--- src/utils/request.js
+++ src/utils/request.js
@@ -15,6 +15,10 @@ service.interceptors.request.use(config => {
   if (store.getters.token) {
     config.headers['X-Token'] = getToken() // 让每个请求携带token-- ['X-Token']为自定义key 请根据实际情况自行修改
   }
+  config.headers.client = window.localStorage.getItem('client')
+  config.headers['access-token'] = window.localStorage.getItem('access-token')
+  config.headers.uid = window.localStorage.getItem('uid')
+  config.headers['token-type'] = window.localStorage.getItem('token-type')
   return config
 }, error => {
   // Do something with request error
@@ -24,7 +28,17 @@ service.interceptors.request.use(config => {
 
 // respone interceptor
 service.interceptors.response.use(
-  response => response,
+  response => {
+    console.log(response.headers)
+    if (response.headers.client) {
+      localStorage.setItem('access-token', response.headers['access-token'])
+      localStorage.setItem('client', response.headers.client)
+      localStorage.setItem('uid', response.headers.uid)
+      localStorage.setItem('token-type', response.headers['token-type'])
+    }
+    return response
+  },
+
   /**
   * 下面的注释为通过response自定义code来标示请求状态，当code返回如下情况为权限有问题，登出并返回到登录页
   * 如通过xmlhttprequest 状态码标识 逻辑可写在下面error中
